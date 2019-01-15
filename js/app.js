49c0149406616d70c7a8553066c308a499f7ac63
Dana Voros <vorosdana@gmail.com>
2019-01-15 12:53:15 -0800

    Moves objects to a constructor and returns to previous functionality

--- js/app.js
+++ js/app.js
@@ -1,210 +1,66 @@
 var salesTable = document.getElementById('sales-data');
+var allCookieStores = [];
+console.log(salesTable.rows);
 
-var locOne = {
-  location: '1st and Pike',
-  minCust: 23,
-  maxCust: 65,
-  avgSales: 6.3,
+function CookieStore(location, minCust, maxCust, avgSales) {
+  this.location= location;
+  this.minCust = minCust;
+  this.maxCust= maxCust;
+  this.avgSales= avgSales;
+  allCookieStores.push(this);
+}
 
-  generateSales: function() {
-    var outputSales = [];
-    for(var i = 0; i < 15; i++){
-      outputSales.push(Math.floor(Math.random() * (this.maxCust-this.minCust)) + this.minCust);
-    }
-    return outputSales;
-  },
 
-  render: function() {
-    // build sales for the day
-    var salesNumbers = this.generateSales();
-
-    // build the table row to work in and put the location in first
-    var tableRow = salesTable.insertRow(salesTable.rows);
-    var locName = document.createElement('td');
-    locName.textContent = this.location;
-    tableRow.appendChild(locName);
-
-    // get var ready to track total sales
-    var salesTotal = 0;
-
-    // add td elements to the table for each hour, and add up total while we're there
-    for(var i = 0; i < 15; i++) {
-      var salesTableOutput = document.createElement('td');
-      salesTableOutput.textContent = salesNumbers[i];
-      tableRow.appendChild(salesTableOutput);
-
-      salesTotal += salesNumbers[i];
-    }
-
-    // add the total to the row
-    var salesTableTotal = document.createElement('td');
-    salesTableTotal.textContent = salesTotal;
-    tableRow.appendChild(salesTableTotal);
-
-    // put the row in the table - didn't work, done properly above
-    // salesTable.appendChild(tableRow);
+CookieStore.prototype.generateSales = function() {
+  var outputSales = [];
+  for(var i = 0; i < 15; i++){
+    outputSales.push(Math.floor(Math.random() * (this.maxCust-this.minCust)) + this.minCust);
   }
+  return outputSales;
 };
 
+CookieStore.prototype.render = function() {
+  // build sales for the day
+  var salesNumbers = this.generateSales();
 
-var locTwo = {
-  location: 'SeaTac Airport',
-  minCust: 3,
-  maxCust: 24,
-  avgSales: 1.2,
-  generateSales: function() {
-    var outputSales = [];
-    for(var i = 0; i < 15; i++){
-      outputSales.push(Math.floor((Math.random() * (this.maxCust-this.minCust))) + this.minCust);
-    }
-    return outputSales;
-  },
-  render: function() {
-    var salesNumbers = this.generateSales();
-    // var tableRow = document.createElement('tr');
-    var tableRow = salesTable.insertRow(salesTable.rows);
-    var locName = document.createElement('td');
-    locName.textContent = this.location;
-    tableRow.appendChild(locName);
+  // build the table row to work in and put the location in first
+  var tableRow = salesTable.insertRow(salesTable.rows.length);
+  var locName = document.createElement('td');
+  locName.textContent = this.location;
+  tableRow.appendChild(locName);
 
-    var salesTotal = 0;
+  // get var ready to track total sales
+  var salesTotal = 0;
 
-    for(var i = 0; i < 15; i++) {
-      var salesTableOutput = document.createElement('td');
-      salesTableOutput.textContent = salesNumbers[i];
-      tableRow.appendChild(salesTableOutput);
+  // add td elements to the table for each hour, and add up total while we're there
+  for(var i = 0; i < 15; i++) {
+    var salesTableOutput = document.createElement('td');
+    salesTableOutput.textContent = salesNumbers[i];
+    tableRow.appendChild(salesTableOutput);
 
-      salesTotal += salesNumbers[i];
-    }
-
-    var salesTableTotal = document.createElement('td');
-    salesTableTotal.textContent = salesTotal;
-    tableRow.appendChild(salesTableTotal);
-
-
-    salesTable.appendChild(tableRow);
+    salesTotal += salesNumbers[i];
   }
+
+  // add the total to the row
+  var salesTableTotal = document.createElement('td');
+  salesTableTotal.textContent = salesTotal;
+  tableRow.appendChild(salesTableTotal);
+
+  // put the row in the table - didn't work, done properly above
+  // salesTable.appendChild(tableRow);
 };
 
-var locThree = {
-  location: 'Seattle Center',
-  minCust: 11,
-  maxCust: 38,
-  avgSales: 3.7,
-  generateSales: function() {
-    var outputSales = [];
-    for(var i = 0; i < 15; i++){
-      outputSales.push(Math.floor((Math.random() * (this.maxCust-this.minCust))) + this.minCust);
-    }
-    return outputSales;
-  },
-  render: function() {
-    var salesNumbers = this.generateSales();
-    // var tableRow = document.createElement('tr');
-    var tableRow = salesTable.insertRow(salesTable.rows);
-    var locName = document.createElement('td');
-    locName.textContent = this.location;
-    tableRow.appendChild(locName);
-
-    var salesTotal = 0;
-
-    for(var i = 0; i < 15; i++) {
-      var salesTableOutput = document.createElement('td');
-      salesTableOutput.textContent = salesNumbers[i];
-      tableRow.appendChild(salesTableOutput);
-
-      salesTotal += salesNumbers[i];
-    }
-
-    var salesTableTotal = document.createElement('td');
-    salesTableTotal.textContent = salesTotal;
-    tableRow.appendChild(salesTableTotal);
-
-
-    salesTable.appendChild(tableRow);
+function renderCookieStores() {
+  for(var i = 0; i < allCookieStores.length; i++)
+  {
+    allCookieStores[i].render();
   }
-};
+}
 
-var locFour = {
-  location: 'Capitol Hill',
-  minCust: 20,
-  maxCust: 38,
-  avgSales: 2.3,
-  generateSales: function() {
-    var outputSales = [];
-    for(var i = 0; i < 15; i++){
-      outputSales.push(Math.floor((Math.random() * (this.maxCust-this.minCust))) + this.minCust);
-    }
-    return outputSales;
-  },
-  render: function() {
-    var salesNumbers = this.generateSales();
-    // var tableRow = document.createElement('tr');
-    var tableRow = salesTable.insertRow(salesTable.rows);
-    var locName = document.createElement('td');
-    locName.textContent = this.location;
-    tableRow.appendChild(locName);
+new CookieStore('1st and Pike', 23, 65, 6.3);
+new CookieStore('SeaTac Airport', 3, 24, 1.2);
+new CookieStore('Seattle Center', 11, 38, 3.7);
+new CookieStore('Capitol Hill', 20, 38, 2.3);
+new CookieStore('Alki', 2, 16, 4.6);
 
-    var salesTotal = 0;
-
-    for(var i = 0; i < 15; i++) {
-      var salesTableOutput = document.createElement('td');
-      salesTableOutput.textContent = salesNumbers[i];
-      tableRow.appendChild(salesTableOutput);
-
-      salesTotal += salesNumbers[i];
-    }
-
-    var salesTableTotal = document.createElement('td');
-    salesTableTotal.textContent = salesTotal;
-    tableRow.appendChild(salesTableTotal);
-
-
-    salesTable.appendChild(tableRow);
-  }
-};
-
-var locFive = {
-  location: 'Alki',
-  minCust: 2,
-  maxCust: 16,
-  avgSales: 4.6,
-  generateSales: function() {
-    var outputSales = [];
-    for(var i = 0; i < 15; i++){
-      outputSales.push(Math.floor((Math.random() * (this.maxCust-this.minCust))) + this.minCust);
-    }
-    return outputSales;
-  },
-  render: function() {
-    var salesNumbers = this.generateSales();
-    // var tableRow = document.createElement('tr');
-    var tableRow = salesTable.insertRow(salesTable.rows);
-    var locName = document.createElement('td');
-    locName.textContent = this.location;
-    tableRow.appendChild(locName);
-
-    var salesTotal = 0;
-
-    for(var i = 0; i < 15; i++) {
-      var salesTableOutput = document.createElement('td');
-      salesTableOutput.textContent = salesNumbers[i];
-      tableRow.appendChild(salesTableOutput);
-
-      salesTotal += salesNumbers[i];
-    }
-
-    var salesTableTotal = document.createElement('td');
-    salesTableTotal.textContent = salesTotal;
-    tableRow.appendChild(salesTableTotal);
-
-
-    salesTable.appendChild(tableRow);
-  }
-};
-
-locOne.render();
-locTwo.render();
-locThree.render();
-locFour.render();
-locFive.render();
+renderCookieStores();
