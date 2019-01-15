cd47483c634d1d0406825f4df4249d8000eeb4fe
Dana Voros <vorosdana@gmail.com>
2019-01-15 13:43:38 -0800

    Constructs bottom row of table

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ function CookieStore(location, minCust, maxCust, avgSales) {
   this.minCust = minCust;
   this.maxCust= maxCust;
   this.avgSales= avgSales;
+  this.salesNumbers = this.generateSales()
   allCookieStores.push(this);
 }
 
@@ -22,7 +23,7 @@ CookieStore.prototype.generateSales = function() {
 
 CookieStore.prototype.render = function() {
   // build sales for the day
-  var salesNumbers = this.generateSales();
+  // var salesNumbers = this.generateSales();
 
   // build the table row to work in and put the location in first
   var tableRow = salesTable.insertRow(salesTable.rows.length);
@@ -36,10 +37,10 @@ CookieStore.prototype.render = function() {
   // add td elements to the table for each hour, and add up total while we're there
   for(var i = 0; i < 14; i++) {
     var salesTableOutput = document.createElement('td');
-    salesTableOutput.textContent = salesNumbers[i];
+    salesTableOutput.textContent = this.salesNumbers[i];
     tableRow.appendChild(salesTableOutput);
 
-    salesTotal += salesNumbers[i];
+    salesTotal += this.salesNumbers[i];
   }
 
   // add the total to the row
@@ -51,6 +52,44 @@ CookieStore.prototype.render = function() {
   // salesTable.appendChild(tableRow);
 };
 
+var hourlyTotalsRow = {
+  totalRow: undefined,
+
+  render: function() {
+    this.totalRow = salesTable.insertRow(salesTable.rows.length);
+
+    var tdEl = document.createElement('td');
+    tdEl.textContent = 'Hourly Totals';
+    this.totalRow.appendChild(tdEl);
+
+    for(var i = 0; i < hours.length + 1; i++)
+    {
+      tdEl = document.createElement('td');
+      this.totalRow.appendChild(tdEl);
+    }
+
+    this.update();
+  },
+
+  update: function() {
+    var dailyTotalAllLoc = 0;
+
+    for(var i = 0; i < hours.length; i++)
+    {
+      var hourlyTotalAllLoc = 0;
+      for(var j = 0; j < allCookieStores.length; j++)
+      {
+        hourlyTotalAllLoc += allCookieStores[j].salesNumbers[i];
+      }
+      dailyTotalAllLoc += hourlyTotalAllLoc;
+
+      salesTable.rows[salesTable.rows.length -1].childNodes[i + 1].textContent = hourlyTotalAllLoc;
+
+    }
+    salesTable.rows[salesTable.rows.length -1].childNodes[salesTable.rows[salesTable.rows.length -1].childNodes.length - 1].textContent = dailyTotalAllLoc;
+  }
+};
+
 function renderCookieStores() {
   for(var i = 0; i < allCookieStores.length; i++)
   {
@@ -60,17 +99,17 @@ function renderCookieStores() {
 
 function buildTitleRow() {
   var trEl = salesTable.insertRow(0);
-  var workingTdEl = document.createElement('th');
-  trEl.appendChild(workingTdEl);
+  var workingThEl = document.createElement('th');
+  trEl.appendChild(workingThEl);
   for(var i = 0; i < hours.length; i++)
   {
-    workingTdEl = document.createElement('th');
-    workingTdEl.textContent = hours[i];
-    trEl.appendChild(workingTdEl);
+    workingThEl = document.createElement('th');
+    workingThEl.textContent = hours[i];
+    trEl.appendChild(workingThEl);
   }
-  workingTdEl = document.createElement('th');
-  workingTdEl.textContent = 'Location Daily Total';
-  trEl.appendChild(workingTdEl);
+  workingThEl = document.createElement('th');
+  workingThEl.textContent = 'Location Daily Total';
+  trEl.appendChild(workingThEl);
 }
 
 new CookieStore('1st and Pike', 23, 65, 6.3);
@@ -81,3 +120,5 @@ new CookieStore('Alki', 2, 16, 4.6);
 
 buildTitleRow();
 renderCookieStores();
+
+hourlyTotalsRow.render();
